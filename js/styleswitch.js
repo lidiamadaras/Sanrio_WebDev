af6c9d84011dbe96f592e47c14b220e2164d6e90
lidiamadaras <125455921+lidiamadaras@users.noreply.github.com>
2024-09-25 12:40:44 +0300

    Add files via upload

---
+++ js/styleswitch.js
@@ -0,0 +1,19 @@
+function addEvent(obj, type, fn) {
+  if (obj.attachEvent) {
+    obj['e' + type + fn] = fn;
+    obj[type + fn] = function() {
+      obj['e' + type + fn](window.event);
+    }
+    obj.attachEvent('on' + type, obj[type + fn]);
+  } else obj.addEventListener(type, fn, false);
+}
+
+function switchStyles() {
+  var selectedOption = this.options[this.selectedIndex],
+    className = selectedOption.value;
+
+  document.body.className = className;
+}
+
+var styleSwitcher = document.getElementById("styleSwitcher");
+addEvent(styleSwitcher, "change", switchStyles);
